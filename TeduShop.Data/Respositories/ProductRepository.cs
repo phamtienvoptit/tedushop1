e44d419560260bc9bd228115a8039731a0b9a48e
phamtienvoptit <phamtienvoptit@gmail.com>
2019-04-15 17:24:10 +0700

    bai7 add SQL

---
+++ TeduShop.Data/Respositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastucture;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Respositories
+{
+    public interface IProductRepository
+    {
+
+    }
+    public class ProductRepository:RepositoryBase<Product>,IProductRepository
+    {
+        public ProductRepository(DbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
