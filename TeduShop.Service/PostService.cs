40a03fab0992c5d090068c87e1e4b6855fe03a33
phamtienvoptit <phamtienvoptit@gmail.com>
2019-04-15 23:24:49 +0700

    add postcategoryservice

--- TeduShop.Service/PostService.cs
+++ TeduShop.Service/PostService.cs
@@ -16,6 +16,7 @@ namespace TeduShop.Service
         void Delete(int id);
         IEnumerable<Post> GetAll();
         IEnumerable<Post> GetAllPaging(int page,int pageSize,out int totalRow);
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId,int page, int pageSize, out int totalRow);
         Post GetById(int id);
         IEnumerable<Post> GetAllByTagPaging(string tag,int page, int pageSize, out int totalRow);
         void SaveChanges();
@@ -46,6 +47,11 @@ namespace TeduShop.Service
         public IEnumerable<Post> GetAll()
         {
             return _postRepository.GetAll(new string[] { "PostCategory"});
+        }
+
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status == true && x.CategoryID == categoryId, out totalRow, page, pageSize,new string[] { "PostCategory"});
         }
 
         public IEnumerable<Post> GetAllByTagPaging(string tag,int page, int pageSize, out int totalRow)
