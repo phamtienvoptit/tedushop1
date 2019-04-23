bfb1f935b0af9d4e0c6fcc877f8f424cac99cc35
phamtienvoptit <phamtienvoptit@gmail.com>
2019-04-23 15:59:27 +0700

    angularjs use scope and rootScope

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,19 @@
+﻿//Khai báo module
+var myApp = angular.module('myModule', []);
+
+myApp.controller("myController", myController);
+myApp.controller("studentController", studentController);
+myApp.controller("schoolController", schoolController);
+//myController.$inject = ['$scope'];
+//Khai báo controller
+function myController($scope) {
+    $scope.message = "This is my message from controller";
+}
+
+function studentController($scope) {
+    $scope.message = "This is my message from student";
+}
+
+function schoolController($scope) {
+    $scope.message = "Annount this my school";
+}
