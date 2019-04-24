5b0f92b9da027c4f005604336f7579b66c47dd54
phamtienvoptit <phamtienvoptit@gmail.com>
2019-04-24 16:39:10 +0700

    add trang quản trị

---
+++ TeduShop.Web/app/components/home/homeController.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../../../assets/admin/libs/angular/angular.js" />
+
+(function (app) {
+    app.controller('homeController', homeController);
+    function homeController() {
+
+    }
+})(angular.module('tedushop'));
